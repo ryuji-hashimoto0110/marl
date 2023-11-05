9907e593a865232d624c00e661e07b1c64727c9d
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2023-11-05 11:25:24 +0900

    enrich comment

--- drl_algos/algorithm.py
+++ drl_algos/algorithm.py
@@ -17,71 +17,54 @@ class Algorithm(ABC):
     RL algorithm must contains
         self.actor (nn.Module)
             .actor.forward(obs: Tensor | dict[AgentID, Tensor]) -> Tensor | dict[AgentID, Tensor]
-            .actor.sample(obs: Tensor | dict[AgentID, Tensor]) -> tuple[Tensor | dict[AgentID, Tensor], float | ndarray]
+            .actor.sample(
+                obs: Tensor | dict[AgentID, Tensor]
+            ) -> tuple[Tensor | dict[AgentID, Tensor], float | dict[AgentID, Tensor]]
     and often contains
         self.critic (nn.Module)
             .critic.forward(
-                obs: 
-                actions: 
-            ) -> 
+                obs: Tensor | dict[AgentID, Tensor]
+                actions: Tensor | dict[AgentID, Tensor]
+            ) -> Tensor
     """
     @torch.no_grad()
     def explore(
         self,
-        obs: ObsType | tuple[dict[AgentID, ObsType]]
-    ) -> tuple[ActionType | dict[AgentID, ActionType], float | ndarray]:
+        obs: ObsType | dict[AgentID, ObsType]
+    ) -> tuple[ActionType | dict[AgentID, ActionType], float | dict[AgentID, Tensor]]:
         """_summary_
 
         Args:
             obs (ObsType | tuple[dict[AgentID, ObsType]]): _description_
 
         Returns:
-            tuple[ActionType | dict[AgentID, ActionType], float | ndarray]: _description_
+            tuple[ActionType | dict[AgentID, ActionType], float | dict[AgentID, Tensor]]: _description_
         """
         obs: Tensor | dict[AgentID, Tensor] = self.convert_obs2tensor(obs)
-        action, log_pi: tuple[Tensor | dict[AgentID, Tensor], float | ndarray] \
-            = self.actor.sample(obs)
-        action: ActionType | dict[AgentID, ActionType] \
-            = self.convert_tensor2action(action)
+        action, log_pi = self.actor.sample(obs)
+        action: ActionType | dict[AgentID, ActionType] = self.convert_tensor2action(action)
         return action, log_pi
 
     @torch.no_grad()
     def exploit(
         self,
-        obs: ObsType | tuple[dict[AgentID, ObsType]]
+        obs: ObsType | dict[AgentID, ObsType]
     ) -> ActionType | dict[AgentID, ActionType]:
         obs: Tensor | dict[AgentID, Tensor] = self.convert_obs2tensor(obs)
         action: Tensor | dict[AgentID, Tensor] = self.actor(obs)
-        action: ActionType | dict[AgentID, ActionType] \
-            = self.convert_tensor2action(action)
+        action: ActionType | dict[AgentID, ActionType] = self.convert_tensor2action(action)
         return action
 
     def convert_obs2tensor(
         self,
-        obs: ObsType | tuple[dict[AgentID, ObsType]]
+        obs: ObsType | dict[AgentID, ObsType]
     ) -> Tensor | dict[AgentID, Tensor]:
-        """_summary_
-
-        Args:
-            obs (ObsType | tuple[dict[AgentID, ObsType]]): _description_
-
-        Returns:
-            Tensor | dict[AgentID, Tensor]: _description_
-        """
         return torch.tensor(obs, dtype=torch.float, device=self.device).unsqueeze_(0)
 
     def convert_tensor2action(
         self,
         action: Tensor | dict[AgentID, Tensor]
     ) -> ActionType | dict[AgentID, ActionType]:
-        """_summary_
-
-        Args:
-            action (Tensor | dict[AgentID, Tensor]): _description_
-
-        Returns:
-            ActionType | dict[AgentID, ActionType]: _description_
-        """
         return action.detach().cpu().numpy()[0]
 
     @abstractmethod
@@ -92,10 +75,10 @@ class Algorithm(ABC):
     def step(
         self,
         env: Env | ParallelEnv,
-        obs: ObsType | tuple[dict[AgentID, ObsType]],
+        obs: ObsType | dict[AgentID, ObsType],
         current_episode_steps: int,
         current_total_steps: int
-    ) -> tuple[ObsType | tuple[dict[AgentID, ObsType]], int]:
+    ) -> tuple[ObsType | dict[AgentID, ObsType], int]:
         pass
 
     @abstractmethod
